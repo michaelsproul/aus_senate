bbb13ae1a03f93fc11c1ef29421fca3e5205a686
Michael Sproul <micsproul@gmail.com>
2017-01-08 13:04:54 +1100

    Accept inevitability of ballot macro.
    Well, not quite. Could just read straight into a vec.
    
    Closes #13.

--- src/parse/prefs2016.rs
+++ src/parse/prefs2016.rs
@@ -19,12 +19,17 @@ pub fn parse_single_ballot(raw_row: csv::Result<PrefRow>, groups: &[Group], cand
     }
 }
 
-// FIXME: This macro is to avoid writing the iterator type. Can use a function once impl Trait lands.
+// NOTE: This macro is to avoid writing the iterator type.
+// One day it may be possible to use `impl Trait`, but at the moment the compiler
+// doesn't understand that the ballots being returned don't depend on the lifetime
+// of the CSV reader's data at all (due to allocations/cloning).
+// See: https://gist.github.com/michaelsproul/20e18f52fc1be2bd05b2767ab6bd166c
 #[macro_export]
 macro_rules! parse_preferences_file {
-    ($reader:expr, $groups:expr, $candidates:expr, $constraints:expr) => {
+    ($reader:expr, $groups:expr, $candidates:expr, $constraints:expr) => {{
+        use $crate::parse::prefs2016::{PrefRow, parse_single_ballot};
         $reader
-            .decode::<$crate::parse::prefs2016::PrefRow>()
-            .map(|raw_row| $crate::parse::prefs2016::parse_single_ballot(raw_row, $groups, $candidates, $constraints))
-    }
+            .decode::<PrefRow>()
+            .map(|raw_row| parse_single_ballot(raw_row, $groups, $candidates, $constraints))
+    }}
 }
